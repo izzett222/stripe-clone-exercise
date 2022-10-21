4fde2cda2c9b639c6347fe38bb9487683291062d
izzett222 <iizzeddin62@gmail.com>
2022-10-21 15:58:27 +0200

    add all the sections for the homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,14 +12,28 @@ module.exports = {
         "dark-blue": "#0a2540",
         "pink-gradient": "#ff333d",
         "purple-gradient": "#a960ee",
-        "yellow-gradient": "#ffcb57"
+        "yellow-gradient": "#ffcb57",
+        "light-gray": "#F6F9FC",
+        "accent": "#635bff",
+        "accent-2": "#00d4ff",
+        "title-color": "#0a2540",
+        "pcolor": "#425466",
+        "body-light": "#adbdcc"
       },
       transformOrigin: {
         "0": "0"
       },
       gridTemplateColumns: {
         "2-col": "2fr 1fr",
+        "3-col-lg": "330px 519px 241px",
+        "3-col": "253px 392px 201px",
         "fluid": "repeat(auto-fill, minmax(248px, 1fr))"
+      },
+      gridTemplateRows: {
+        "4-col-lg": "90px 215px 128px 549px",
+        "4-col": "70px 157px 97px 333px",
+        "footer": "repeat(3, fit-content)"
+
       },
       backgroundImage: {
         "desk": "linear-gradient(hsla(0,0%,100%,.4),hsla(0,0%,100%,.3) 25%,rgba(246,249,252,.3) 50%,#f6f9fc 60%)"
@@ -44,13 +58,25 @@ module.exports = {
       height: {
         "32": "32.5rem",
         "58": "929px",
-        "fluid": "40vw"
+        "fluid": "40vw",
+        "near-full": "98%"
       },
       margin: {
         "31": "126px"
       },
       padding: {
         "7/10": "70%"
+      },
+      borderRadius: {
+        "4xl": "30px",
+        "5xl": "40px",
+        "6xl": "50px"
+      },
+      skew: {
+        "4": "4deg",
+      },
+      fontSize: {
+        "15": "15px"
       }
     },
   },
